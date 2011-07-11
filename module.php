54ae3660daa16e72f6927665369aabe4f24d1a75
mykiimike <mv@verman.fr>
2011-07-11 06:43:32 +0000

    Release 1.2.2

--- module.php
+++ module.php
@@ -7,8 +7,8 @@ class wfm_god extends wf_module {
 	
 	public function get_name() { return("god"); }
 	public function get_description()  { return("OWF Native God module"); }
-	public function get_banner()  { return("god/1.2.2"); }
-	public function get_version() { return("1.2.2"); }
+	public function get_banner()  { return("god/1.2.3"); }
+	public function get_version() { return("1.2.3"); }
 	public function get_authors() { return("Michael VERGOZ"); }
 	public function get_depends() { return(NULL); }
 	
